a35b6fad3c295db8cee619147f2c1f5e79ea5009
Norman Kuusik <Norman Kuusik>
2021-07-29 12:45:07 +0000

    Change chart line border widith and colour.

--- src/subcomponents/LineChart.js
+++ src/subcomponents/LineChart.js
@@ -14,13 +14,12 @@ class LineChart extends React.Component {
         label: `# of points in season ${season}`,
         data: this.props.team.seasons[season],
         backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(75, 192, 192)',
-        borderWidth: 1,
+        borderColor: 'rgb(0, 10, 12)',
+        borderWidth: 1.5,
         tension: 0.5
       }
       dataSets.push(singleDataSet);
     }
-    console.log(dataSets)
     return dataSets;
   }
 
@@ -66,7 +65,6 @@ class LineChart extends React.Component {
 function calculateLabels(seasonsAsObject) {
   let longestSeason = null;
   for (let seasonKey in seasonsAsObject) {
-    console.log(seasonKey);
     if (longestSeason == null || seasonsAsObject[seasonKey].length > longestSeason.length) {
       longestSeason = seasonsAsObject[seasonKey];
     }
@@ -75,7 +73,6 @@ function calculateLabels(seasonsAsObject) {
   for (let i = 1; i < longestSeason.length; i++) {
       labels.push(`Game #${i}`);
   }
-  console.log(longestSeason);
   return labels;
 }
 
