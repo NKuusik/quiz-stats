d2284c7538326e8f412a082d0cb4923cf3964f1a
Norman Kuusik <Norman Kuusik>
2021-07-30 22:29:42 +0000

    Add header with buttons to choose between Team and Season view modes.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ function Header(props) {
     return(
         <div className={styles["header"]}>
             <ul>
-                <li><h2 onClick={() => {props.choice("team")}}>Check single team accross seasons.</h2></li>
-                <li><h2 onClick={() => {props.choice("season")}}>Check single season in comparison to teams</h2></li>
+                <li><h2 onClick={() => {props.choice("team")}}>Check team</h2></li>
+                <li><h2 onClick={() => {props.choice("season")}}>Check season</h2></li>
             </ul>
         </div>
     );
