b0199204deda94bb5c4252d70a886e4f2c18a775
Norman Kuusik <Norman Kuusik>
2022-01-19 18:57:04 +0000

    Add search field to MenuBar that can recognize matching team names in the menu bar.

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../style.css';
+import SearchField from '../subcomponents/SearchField';
 
 type MyProps = {
   category : Object;
@@ -14,10 +15,13 @@ class MenuBar extends React.Component<MyProps> {
     };
   }
 
+
   render () {
+    const teamNames = Object.keys(this.props.category);
     return (
             <div>
               <h1>Choose which team/season you want to check.</h1>
+              <SearchField teamNames={teamNames}/>
                 <div >
                 {Object.values(this.props.category).map(entryInCategory => (
                 <div key={ entryInCategory.name }className={styles['entry-selection']} onClick={() => this.props.choice(entryInCategory)}>
---
+++ src/subcomponents/SearchField.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+type MyProps = {
+    teamNames: string[]
+}
+
+const SearchField = ({teamNames}: MyProps) => {
+    const [searchFieldValue, setSearchFieldValue] = useState("");
+
+    const handleChange = (event) => {
+        setSearchFieldValue(event.target.value);
+    }
+    
+    function search() {
+        findMatchingNames(searchFieldValue, teamNames); // Optimeerime nii, et ainult siis otsime, kui v2ljal rohkem kui 1 t2hte.
+    }
+
+    function findMatchingNames(searchInput: string, names: string[]) {
+        let foundMatches : string[] = [];
+        for (let entry of names) {
+            if (entry.includes(searchInput)) {
+                foundMatches.push(entry);
+            }
+        }
+        console.log("Found following matches with " + searchInput);
+        console.log(foundMatches);
+        return foundMatches;
+    }
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder='Search...' value={searchFieldValue} onChange={handleChange} onKeyUp={search}/>
+            </form>
+        </div>
+    )
+}
+
+export default SearchField;
