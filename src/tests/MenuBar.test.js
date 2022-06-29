cf1325e1b2d739ec1e8a4856fd17140d074c2113
normankuusik@gmail.com <normankuusik@gmail.com>
2022-06-29 21:05:58 +0300

    Add tests for MenuBar.

--- src/tests/MenuBar.test.js
+++ src/tests/MenuBar.test.js
@@ -1,6 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
 import renderer from 'react-test-renderer';
 import MenuBar from '../components/MenuBar';
+import {Team} from '../classes/Team';
+import {render, fireEvent} from '@testing-library/react';
 
 const category = {};
 const choice = () => {};
@@ -29,3 +35,34 @@ it('team renders correctly', () => {
          .toJSON();
        expect(tree).toMatchSnapshot(); 
      });
+
+test('value changes in SearchField input', () => { // Triviaalne test, aga kui jatta, siis äkki SearchFieldi sees?
+  const menuBar = render((<MenuBar viewType="" category={category} choice={choice}/>));
+  const searchField = menuBar.container.querySelector('input');
+  expect(searchField.value).toBe('');
+  fireEvent.change(searchField, {target: {value: 'gad'}});
+  expect(searchField.value).toBe('gad');
+});
+
+test('entries are shown', () => {
+  const firstTeam = new Team(1, "Fake team", [1, 2], 3);
+  firstTeam.seasons["SeasonNumber"] = [3, 4]
+
+  const secondTeam = new Team(1, "secondTeam", [1, 2], 3);
+  firstTeam.seasons["SeasonNumber"] = [3, 4]
+  secondTeam.seasons["SeasonNumber"] = [3, 4]
+  
+  let testTeams = {
+    "Fake team": firstTeam,
+    "secondTeam": secondTeam
+  };
+  const menuBar = render((<MenuBar viewType="team" category={testTeams} choice={choice}/>));
+  const entriesAsDOM = menuBar.container.getElementsByClassName('entry-selection');
+  expect(entriesAsDOM.length).toBe(2);
+  expect(entriesAsDOM[0].textContent).toBe('Fake team');
+  expect(entriesAsDOM[1].textContent).toBe('secondTeam');
+});
+
+test('entries correspond to value on SearchField input', () => {
+
+});
--- src/tests/TeamViewWrapper.test.js
+++ src/tests/TeamViewWrapper.test.js
@@ -6,7 +6,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import TeamViewWrapper from '../components/TeamViewWrapper';
 import {Team} from '../classes/Team';
-import {screen} from '@testing-library/dom';
 import {render, fireEvent} from '@testing-library/react';
 
 const testTeam = new Team(1, "Fake team", [1, 2], 3);
@@ -28,7 +27,6 @@ test("Active team is changed in menu bar", () => {
   const element = render(<TeamViewWrapper teams={testTeams} seasonNames={['SeasonNumber']}/>);
   const entriesInMenuBar = element.container.querySelector('.entry-selection');
   fireEvent.click(entriesInMenuBar);
-  console.log(element);
   expect(element.getByRole('heading')).toHaveTextContent('Stats for team Fake team');
 });  
 
