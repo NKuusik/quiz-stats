3d1fb72f8cfdc7f5278c0c2e61b283825d54f0bc
normankuusik@gmail.com <normankuusik@gmail.com>
2022-07-01 13:52:37 +0300

    Set default activeSeason value with null in SeasonViewWrapper

--- src/components/SeasonViewWrapper.tsx
+++ src/components/SeasonViewWrapper.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import MenuBar from './MenuBar';
 import SeasonView from './SeasonView';
 import styles from '../style.css';
-import { Team }  from '../classes/Team';
 import { Season } from '../classes/Season';
 
 type MyProps = {
@@ -10,18 +9,17 @@ type MyProps = {
 }
 
 function SeasonViewWrapper ({ seasons } : MyProps) {
-  const defaultState = new Season("", "", 0, []) // Motle, kas on parem variant. = muuda vajadusel klasside struktuure
-  const [activeSeason, setActiveSeason] = useState(defaultState);
+  const [activeSeason, setActiveSeason] = useState<Season | null>(null);
 
   function chooseSeason(chosenSeason: Season) {
     if (activeSeason === chosenSeason) {
-      setActiveSeason(defaultState);
+      setActiveSeason(null);
     } else {
       setActiveSeason(chosenSeason);
     }
   }
   let seasonView;
-  if (activeSeason != defaultState) {
+  if (activeSeason != null) {
     seasonView = <SeasonView season={activeSeason} />;
   }
 
