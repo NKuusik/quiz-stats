d910694580bb61c80c96c70ca3cba803d8e68254
Norman Kuusik <normankuusik@gmail.com>
2024-09-16 12:05:17 +0300

    Add data for seasons 12 and 13.

--- src/resources/seasons.js
+++ src/resources/seasons.js
@@ -6,5 +6,7 @@ import season8 from './8.season.csv';
 import season9 from './9.season.csv';
 import season10 from './10.season.csv';
 import season11 from './11.season.csv';
+import season12 from './12.season.csv';
+import season13 from './13.season.csv';
 
-export {season4, season5, season6, season7, season8, season9, season10, season11};
+export {season4, season5, season6, season7, season8, season9, season10, season11, season12, season13};
