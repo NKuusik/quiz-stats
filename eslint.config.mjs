22d2e4bd7c04c82e83ea37841d684cc12139bf14
Norman Kuusik <normankuusik@gmail.com>
2024-09-17 12:57:03 +0300

    Migrate to new ReactDOM.createRoot() API. Specify react version in eslint configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {ignores: ["**/*.json", "**/*.scss", "**/*.css", "**/*.csv", "**/*.txt", "src/tests", "src/style.css.d.ts"]},
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {settings: {react:{version:"detect"}}},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as rawData from './resources/seasons';
 
-ReactDOM.render(
-  <React.StrictMode>
-      <App rawData={rawData} collapseWidth={800}/>  
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+<App rawData={rawData} collapseWidth={800} />
 );
 
 reportWebVitals();
